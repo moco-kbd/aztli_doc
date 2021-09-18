ac4f1279ae0b8dc074010d0d48b2870be5f0c263
moco <moco.kbd@gmail.com>
2021-09-19 02:47:38 +0900

    first commit

---
+++ firmware/aztli/keymaps/via/keymap.c
@@ -0,0 +1,74 @@
+/* Copyright 2021 moco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN1,
+	_FN2,
+	_FN3
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        XXXXXXX, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX,
+        KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_DEL,  KC_END,  KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        XXXXXXX, KC_UP,   XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_P0,   KC_PDOT, KC_PENT
+    ),
+    [_FN1] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_FN2] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_FN3] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______
+    )
+};
+
+#ifdef ENCODER_ENABLE
+
+/* Rotary encoder settings */
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+                tap_code16(clockwise ? KC_VOLU : KC_VOLD);
+    } else if (index == 1) {
+                tap_code16(clockwise ? KC_MNXT : KC_MPRV);
+    }
+    return true;
+}
+
+#endif
